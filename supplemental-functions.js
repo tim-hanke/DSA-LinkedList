45a696c843f82403484ce22fbbacb12570ce093e
Timothy Hanke <tim.hanke@gmail.com>
2021-03-10 08:56:51 -0500

    added the supplemental function 'size'

--- supplemental-functions.js
+++ supplemental-functions.js
@@ -17,9 +17,30 @@ const supplementalFunctions = {
 
     console.log(listArray.join(",\n"));
   },
+  size(SLL) {
+    // if there is no head, then the list has zero size
+    if (!SLL.head) {
+      return 0;
+    }
+
+    // if there is a head, then the linked list has
+    // at least one entry, so we'll start at one
+    let listSize = 1;
+
+    let currNode = SLL.head;
+
+    // while there are still more nodes, we add to listSize
+    while (currNode.next !== null) {
+      listSize++;
+      currNode = currNode.next;
+    }
+
+    return listSize;
+  },
 };
 
 const main = require("./singly-linked");
 
 let SLL = main();
 supplementalFunctions.display(SLL);
+console.log(supplementalFunctions.size(SLL));
